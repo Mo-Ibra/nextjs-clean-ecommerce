2a1ffabfae47d1d2e0102f22f7d26c6ffd5fb9f6
Mo-Ibra <mohamedelgedwy245@gmail.com>
2025-05-14 21:49:32 +0300

    Fix Issue In Mobile Navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,11 +2,12 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Search, ShoppingBag } from "lucide-react";
+import { Menu, Search, ShoppingBag } from "lucide-react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import CartSheet from "./cart-sheet";
+import { Sheet, SheetContent, SheetTitle, SheetTrigger } from "./ui/sheet";
 
 /**
  * @description The main navigation component.
@@ -23,12 +24,12 @@ export default function Navbar() {
    * @param {React.FormEvent} e - Event object.
    */
   const handleSearch = (e: React.FormEvent) => {
-    e.preventDefault()
+    e.preventDefault();
     if (searchQuery) {
       // Redirect to search results page with query parameter.
       router.push(`/search?q=${encodeURIComponent(searchQuery)}`);
     }
-  }
+  };
 
   const routes = [
     { href: "/", label: "Home" },
@@ -43,7 +44,30 @@ export default function Navbar() {
       <div className="container mx-auto md:py-6 md:px-8 flex h-16 items-center">
 
         {/* Mobile Navigation */}
-        <div className="md:hidden"></div>
+        <div className="md:hidden">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="ghost" size="icon">
+                <Menu className="h-6 w-6" />
+                <span className="sr-only">Toggle menu</span>
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="w-[300px] sm:w-[400px]">
+              <SheetTitle className="text-sm text-bold hidden">Menu</SheetTitle>
+              <nav className="flex flex-col gap-4 mt-8 px-12">
+                {routes.map((route) => (
+                  <Link
+                    key={route.href}
+                    href={route.href}
+                    className="text-lg font-medium transition-colors hover:text-primary"
+                  >
+                    {route.label}
+                  </Link>
+                ))}
+              </nav>
+            </SheetContent>
+          </Sheet>
+        </div>
 
         {/* Logo */}
         <Link className="flex items-center gap-2 ml-4 md:ml-0 md:mr-8" href="/">
@@ -80,14 +104,18 @@ export default function Navbar() {
           </form>
 
           {/* Mobile Search */}
-          <Button variant="ghost" size="icon" className="md:hidden" onClick={() => router.push('/search')} >
+          <Button
+            variant="ghost"
+            size="icon"
+            className="md:hidden"
+            onClick={() => router.push("/search")}
+          >
             <Search className="w-5 h-5" />
           </Button>
 
           {/* Sheet */}
 
           <CartSheet />
-
         </div>
       </div>
     </header>
